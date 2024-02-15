3a7043475cb2e7a89a3486375c86409ec20b4e96
Anuj <anujverma3553@gmail.com>
2024-02-15 19:33:40 +0530

    Creating Login Endpoint & sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,15 +9,17 @@ const { body, validationResult } = require('express-validator')
 
 const JWT_SECRET = "mern$Open$SourceProject";
 
-// Create a User using : POST: "/api/auth". Does'nt require Auth
+// Create a User using : POST: "/api/auth/createuser". Does'nt require Auth
 router.post('/createuser', [
     // creating check vadilation for user credentials like name, email and password  
+
     // Name must be at least 3 chars long
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     // Email must be an email
     body('email', 'Enter a valid email').isEmail(),
     // Password must be at least 5 chars long
     body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
+
 ], async (req, res) => {
 
     // For request into the body - Go to thunderclient and select in header the content type and application/json
@@ -32,34 +34,38 @@ router.post('/createuser', [
             // Return a status 400 and return json of error in the array form
             return res.status(400).json({ errors: errors.array() });
         }
-        
+
+        // Below line is promise so await it
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
+
+        // Below line is promise so await it
         const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password , salt);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
         // Create a new user. This code return a promise --> Check whether the user with this mail exists already 
         user = await User.create({
             name: req.body.name,
             password: secPass,
             email: req.body.email,
         })
-        
+
+        // Define the data to sign the data with JWT_SECRET
         const data = {
-            user:{
-                id:user.id
+            user: {
+                id: user.id
             }
         }
 
+        // Sign the data and give the authtoken to the user
         const authtoken = jwt.sign(data, JWT_SECRET);
-        res.json({authtoken})
-
-        
+        res.json({ authtoken })
     }
     catch (error) {
         console.log(error.message)
-        res.status(500).send("Some error occured")
+        res.status(500).send("Internal Server Error")
     }
 
     // // Takes user argument send response json of user --> When creates a new user
@@ -70,6 +76,57 @@ router.post('/createuser', [
     //     // Send the error in json (error, message) --> If user existing
     //     res.json({error: "Please Enter an unique value for email", message: error.message})}
     // })
+})
+
+// Create a User using : POST: "/api/auth/login". Does'nt require Auth
+router.post('/login', [
+    // creating check vadilation for user credentials like name, email and password  
+
+    // Email must be an email
+    body('email', 'Enter a valid email').isEmail(),
+    // Password must be at least 5 chars long
+    body('password', 'Password cannot be blank').exists(),
+
+], async (req, res) => {
+
+    // If there are errors, return Bad request and the erors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // Return a status 400 and return json of error in the array form
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        // Below line is promise so await it
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" })
+        }
+
+        // Below line is promise so await it
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" })
+        }
+
+        // Define the data to sign the data with JWT_SECRET
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        // Sign the data and give the authtoken to the user
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({ authtoken })
+
+    }
+    catch (error) {
+        console.log(error.message)
+        res.status(500).send("Internal Server Error")
+    }
 })
 
 module.exports = router 
