a30789c256ff6d678e5e0e753ca97538e0bcab2d
Anuj <anujverma3553@gmail.com>
2024-02-15 17:47:36 +0530

    Hashing Passwords using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,13 @@
 const express = require('express');
+var jwt = require('jsonwebtoken');
 const User = require('../Models/User')
+const bcrypt = require('bcrypt');
 const router = express.Router();
+
+require('dotenv').config()
 const { body, validationResult } = require('express-validator')
+
+const JWT_SECRET = "mern$Open$SourceProject";
 
 // Create a User using : POST: "/api/auth". Does'nt require Auth
 router.post('/createuser', [
@@ -19,7 +25,6 @@ router.post('/createuser', [
     // const user = User(req.body)
     // user.save()
 
-
     try {
         // If there are errors, return Bad request and the erors
         const errors = validationResult(req);
@@ -27,22 +32,36 @@ router.post('/createuser', [
             // Return a status 400 and return json of error in the array form
             return res.status(400).json({ errors: errors.array() });
         }
-        // Create a new user. This code return a promise --> Check whether the user with this mail exists already 
+        
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password , salt);
+        // Create a new user. This code return a promise --> Check whether the user with this mail exists already 
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
         })
-        res.json(user)
-    } 
+        
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken})
+
+        
+    }
     catch (error) {
         console.log(error.message)
         res.status(500).send("Some error occured")
     }
+
     // // Takes user argument send response json of user --> When creates a new user
     // .then((user) => res.json(user)).
     // catch((error)=>{
