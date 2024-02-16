41a7514fb7146a8d715384c4f4043578a3893fc3
Anuj <anujverma3553@gmail.com>
2024-02-16 23:02:13 +0530

    OpenSource React Part : Fetching projects from Context API

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,10 +1,17 @@
-
 import Upload from "./Upload"
+import { useContext } from 'react'
+import projectContext from '../context/projectContext';
 
 function Home() {
+  const context = useContext(projectContext)
+  const { projects, setprojects } = context;
   return (
     <div>
-      <Upload title="Click Here To Upload👇" desc="Upload"/>
+      <Upload title="Click Here To Upload👇" desc="Upload" />
+      <h2>Your Projects</h2>
+      {projects.map((project) => {
+        return project.title;
+      })}
     </div>
   )
 }
--- client/src/context/ProjectState.jsx
+++ client/src/context/ProjectState.jsx
@@ -1,16 +1,32 @@
 import PropTypes from 'prop-types';
 import projectContext from "./projectContext.jsx";
-// import { useState } from 'react';
-
+import { useState } from 'react';
 const ProjectStates = (props) => {
-  // const [project, setproject] = useState(0)
-  
-  const state = {
-    "name": "Anuj",
-    "class": "5b"
-  }
+  const projectsInitial = [
+    {
+      "_id": "65cf7b6105eab042ff65d0b2",
+      "user": "65cf7b2305eab042ff65d0b0",
+      "title": "Mr Anuj",
+      "description": "My Anuj Desc",
+      "link": "https://www.instagram.com/",
+      "__v": 0,
+      "date": "2024-02-16T17:02:49.634Z"
+    },
+    {
+      "_id": "65cf7b6d05eab042ff65d0b4",
+      "user": "65cf7b2305eab042ff65d0b0",
+      "title": "Mr Anuj",
+      "description": "My Anuj Desc",
+      "link": "https://www.gihub.com/",
+      "__v": 0,
+      "date": "2024-02-16T17:02:49.635Z"
+    }
+  ]
+
+  const [projects, setprojects] = useState(projectsInitial)
+
   return (
-    <projectContext.Provider value={state}>
+    <projectContext.Provider value={{projects, setprojects}}>
       {props.children}
     </projectContext.Provider>
   )
