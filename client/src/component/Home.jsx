e088b372dc1bed249300f749ee3400362f5e6ced
Harshit <harshit28k@gmail.com>
2024-03-15 12:46:24 +0530

    Add styling in Home Component

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import image from '../assets/images/projects.png'
+import image1 from '../assets/images/Anuj.jpeg'
+import image2 from '../assets/images/jitendra.jpeg'
+import image3 from '../assets/images/harshit.jpeg'
 import './Home.css'
 
 const Home = () => {
@@ -19,91 +21,90 @@ const Home = () => {
                         <Link role="button" to='/login' className="button-color2">Login</Link>
                     </div>
                 </div>
-                <div id="carouselExampleCaptions" className="carousel slide">
-                    <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src={image} alt="..." />
-                            <div className="carousel-caption">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={image} alt="..." />
-                            <div className="carousel-caption">
-                                <h5>Second slide label</h5>
-                                <p>Some representative placeholder content for the second slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={image} alt="..." />
-                            <div className="carousel-caption">
-                                <h5>Third slide label</h5>
-                                <p>Some representative placeholder content for the third slide.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div>
 
-                <section className='section-card'>
-                    <h3>Be a part of Kaiyuan Community</h3>
-                    <div className="row">
-                        <div className="col-sm-6 mb-3 mb-sm-0">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                <section className='my-4 mx-2'>
+                    <div id="carouselExampleAutoplaying" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                        <div className="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" style={{ backgroundColor: "currentcolor" }} className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" style={{ backgroundColor: "currentcolor" }} aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" style={{ backgroundColor: "currentcolor" }} aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src={image1} className="d-block w-50 m-auto" alt="img1" />
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={image2} className="d-block w-50 m-auto" alt="img2" />
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>Second slide label</h5>
+                                    <p>Some representative placeholder content for the second slide.</p>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={image3} className="d-block w-50 m-auto" alt="img3" />
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>Third slide label</h5>
+                                    <p>Some representative placeholder content for the third slide.</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-sm-6">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true" style={{ backgroundColor: "currentcolor" }}></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true" style={{ backgroundColor: "currentcolor" }}></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
                     </div>
-                    <h3>Be a part of Kaiyuan Community</h3>
-
-                    <div className="row">
-                        <div className="col-sm-6 mb-3 mb-sm-0">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </section>
+
+                <section className='CARD-INFO-sec'>
+                    <h2 className='Heading-page text-center mt-5'>Be a part of Open Source Community</h2>
+                    <div className="CARD-INFOs">
+                        <div className="CARD-INFO-row1">
+                            <div className="CARD-INFO">
+                                <h3>Start your journey</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Rerum corporis tenetur iste illo ipsum quaerat illum repellendus
+                                    libero minima dolores eos quos hic,
+                                    vero maxime perferendis sint a amet. Nobis.</p>
+                                <button className='btn-box'><a href="">Apply as contributor</a></button>
+                            </div>
+                            <div className="CARD-INFO">
+                                <h3>Start your journey</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Rerum corporis tenetur iste illo ipsum quaerat illum repellendus
+                                    libero minima dolores eos quos hic,
+                                    vero maxime perferendis sint a amet. Nobis.</p>
+                                <button className='btn-box'><a href="">Apply as contributor</a></button>
+                            </div>
+                        </div>
+                        <div className="CARD-INFO-row2">
+                            <div className="CARD-INFO">
+                                <h3>Start your journey</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Rerum corporis tenetur iste illo ipsum quaerat illum repellendus
+                                    libero minima dolores eos quos hic,
+                                    vero maxime perferendis sint a amet. Nobis.</p>
+                                <button className='btn-box'><a href="">Apply as contributor</a></button>
+                            </div>
+                            <div className="CARD-INFO">
+                                <h3>Start your journey</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Rerum corporis tenetur iste illo ipsum quaerat illum repellendus
+                                    libero minima dolores eos quos hic,
+                                    vero maxime perferendis sint a amet. Nobis.</p>
+                                <button className='btn-box'><a href="">Apply as contributor</a></button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
                 <section className='section-card'>
                     <h3>New Update</h3>
                     <div className="card">
