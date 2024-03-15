0681b15226d97f580a1b766fcb34e808b2cedb34
Harshit <harshit28k@gmail.com>
2024-03-15 13:12:38 +0530

    Crousol updates in home component

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -21,47 +21,50 @@ const Home = () => {
                         <Link role="button" to='/login' className="button-color2">Login</Link>
                     </div>
                 </div>
+            </div>
 
-                <section className='my-4 mx-2'>
-                    <div id="carouselExampleAutoplaying" className="carousel slide carousel-fade" data-bs-ride="carousel">
-                        <div className="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" style={{ backgroundColor: "currentcolor" }} className="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" style={{ backgroundColor: "currentcolor" }} aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" style={{ backgroundColor: "currentcolor" }} aria-label="Slide 3"></button>
+                <div className="Container-Carousel">
+                    <section className='my-4 mx-2'>
+                        <div id="carouselExampleAutoplaying" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                            <div className="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" style={{ backgroundColor: "currentcolor" }} className="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" style={{ backgroundColor: "currentcolor" }} aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" style={{ backgroundColor: "currentcolor" }} aria-label="Slide 3"></button>
+                            </div>
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src={image1} className="d-block m-auto" alt="img1" />
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>First slide label</h5>
+                                        <p>Some representative placeholder content for the first slide.</p>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src={image2} className="d-block m-auto" alt="img2" />
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Second slide label</h5>
+                                        <p>Some representative placeholder content for the second slide.</p>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src={image3} className="d-block m-auto" alt="img3" />
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Third slide label</h5>
+                                        <p>Some representative placeholder content for the third slide.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true" style={{ backgroundColor: "currentcolor" }}></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true" style={{ backgroundColor: "currentcolor" }}></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
                         </div>
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src={image1} className="d-block w-50 m-auto" alt="img1" />
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>First slide label</h5>
-                                    <p>Some representative placeholder content for the first slide.</p>
-                                </div>
-                            </div>
-                            <div className="carousel-item">
-                                <img src={image2} className="d-block w-50 m-auto" alt="img2" />
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Second slide label</h5>
-                                    <p>Some representative placeholder content for the second slide.</p>
-                                </div>
-                            </div>
-                            <div className="carousel-item">
-                                <img src={image3} className="d-block w-50 m-auto" alt="img3" />
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Third slide label</h5>
-                                    <p>Some representative placeholder content for the third slide.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true" style={{ backgroundColor: "currentcolor" }}></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true" style={{ backgroundColor: "currentcolor" }}></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
-                    </div>
-                </section>
+                    </section>
+                </div>
 
                 <section className='CARD-INFO-sec'>
                     <h2 className='Heading-page text-center mt-5'>Be a part of Kaiyuan Community</h2>
@@ -100,7 +103,7 @@ const Home = () => {
                 <section className='section-card'>
                     <h3>New Update</h3>
                     <div className="card text-center">
-                        <h3> <a href="#">About-Us</a></h3>
+                        <h3> <Link to="/about">About-Us</Link></h3>
                         <div className="card-body">
                             <h5 className="card-title">Special title treatment</h5>
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -109,7 +112,6 @@ const Home = () => {
                     </div>
                 </section>
             </div>
-        </div>
     )
 }
 
