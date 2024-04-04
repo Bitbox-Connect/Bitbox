3f76a4f23c7804b28ce5c704bc565dc736b4bfa0
Anuj <anujverma3553@gmail.com>
2024-04-04 21:27:30 +0530

    Resolve code issues

--- client/src/component/Signup.jsx
+++ client/src/component/Signup.jsx
@@ -91,14 +91,12 @@ const Signup = (props) => {
               <button type="submit" className="btn btn-primary">Signup</button>
             </div>
           </form>
-          {!value ?(<><p>hello</p></>)
-          :(
+          {!value ? <p></p> :
             <button className="social-button google" onClick={handleClick}>
               <svg className="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512">
                 <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" fill="#4285F4"></path>
               </svg>
             </button>
-          )
           }
         </div>
         <h5 className='text-center'>or</h5>
