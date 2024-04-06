3314b8f90524a6bbed2bf1dca1b7af57ca41f031
technical-jitendra <jitendrakumarjjk10@gmail.com>
2024-04-07 00:25:56 +0530

    feedback form

--- client/src/component/Footer/Feedback.jsx
+++ client/src/component/Footer/Feedback.jsx
@@ -1,7 +1,86 @@
+import React from "react";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardFooter,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRadio,
+  MDBRow,
+  MDBTextArea,
+} from "mdb-react-ui-kit";
+
 export default function Feedback() {
   return (
-    <div className='container'>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, possimus? Quasi, unde laudantium animi autem porro dolorum aliquid commodi laborum, quibusdam libero excepturi. Assumenda beatae itaque, excepturi architecto modi vero!
-    </div>
-  )
+    <MDBContainer>
+      <MDBRow className="justify-content-center">
+        <MDBCol size="6">
+          <MDBCard>
+            <MDBCardBody>
+              <div className="text-center">
+                <MDBIcon far icon="file-alt" className="mb-3 text-primary" size="4x" />
+                <p>
+                  <strong>Your opinion matters</strong>
+                </p>
+                <p>
+                  Have some ideas on how to improve our product?
+                  <strong> Give us your feedback.</strong>
+                </p>
+              </div>
+
+              <hr />
+
+              <form className="px-4">
+                <p className="text-center">
+                  <strong>Your rating:</strong>
+                </p>
+                <MDBRadio
+                  name="flexRadioDefault"
+                  id="flexRadioDefault1"
+                  label="Very good"
+                  className="mb-2"
+                  defaultChecked
+                />
+                <MDBRadio
+                  name="flexRadioDefault"
+                  id="flexRadioDefault2"
+                  label="Good"
+                  className="mb-2"
+                />
+                <MDBRadio
+                  name="flexRadioDefault"
+                  id="flexRadioDefault3"
+                  label="Mediocre"
+                  className="mb-2"
+                />
+                <MDBRadio
+                  name="flexRadioDefault"
+                  id="flexRadioDefault4"
+                  label="Bad"
+                  className="mb-2"
+                />
+                <MDBRadio
+                  name="flexRadioDefault"
+                  id="flexRadioDefault5"
+                  label="Very bad"
+                  className="mb-2"
+                />
+                <p className="text-center">
+                  <strong>What could we improve?</strong>
+                </p>
+                <MDBTextArea className="mb-4" id="textAreaExample" label="Message" rows={4} />
+              </form>
+            </MDBCardBody>
+            <MDBCardFooter>
+              <div className="text-end">
+                <MDBBtn>Submit</MDBBtn>
+              </div>
+            </MDBCardFooter>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 }
