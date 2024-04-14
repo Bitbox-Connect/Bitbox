12c7fae7fefdbe98f8b64e2c6c11951167921e13
technical-jitendra <jitendrakumarjjk10@gmail.com>
2024-04-14 13:28:56 +0530

    footer updates

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -44,10 +44,10 @@ function Footer(props) {
           <div className='fs-5'>
             <h3 className='fw-bold fs-2'>Follow us on</h3>
             <ul>
-              <li><a href="https://www.linkedin.com/in/bit-box-community"><FaLinkedin color="#0077b5" fontSize="2rem" />Linkedin</a></li>
-              <li><a href="https://twitter.com/home"><FaTwitter color="#1da1f2" fontSize="2rem" />Twitter</a></li>
-              <li><a href="https://github.com/bitboxcommunity"><FaGithub color="#211F1F" fontSize="2rem" />GitHub</a></li>
-              <li><a href="https://www.facebook.com/bit-box-community"><FaFacebookSquare color="#3b5998" fontSize="2rem" />Facebook</a></li>
+              <li><a href="https://www.linkedin.com/in/bit-box-community" target='blank'><FaLinkedin color="#0077b5" fontSize="2rem" />Linkedin</a></li>
+              <li><a href="https://twitter.com/BITBOX688152" target='blank-1'><FaTwitter color="#1da1f2" fontSize="2rem" />Twitter</a></li>
+              <li><a href="https://github.com/bitboxcommunity" target='blank-2'><FaGithub color="#211F1F" fontSize="2rem" />GitHub</a></li>
+              <li><a href="https://www.facebook.com/bit-box-community" target='blank-3'><FaFacebookSquare color="#3b5998" fontSize="2rem" />Facebook</a></li>
             </ul>
           </div>
         </div>
