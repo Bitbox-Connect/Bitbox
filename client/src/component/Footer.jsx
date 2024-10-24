3f4378ff700783920f0170a903df93f0a47991f7
Anuj3553 <anujverma3553@gmail.com>
2024-10-24 14:55:55 +0530

    Name Change

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../component/css/footer.css'
+import '../component/css/Footer.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FaXTwitter } from "react-icons/fa6";
