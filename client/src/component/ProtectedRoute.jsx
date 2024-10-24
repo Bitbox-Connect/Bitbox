dce509cf988d9b862a8f2b17cbe149d3b8e70c3d
2004vivek <119566938+2004vivek@users.noreply.github.com>
2024-10-24 18:13:12 +0530

    Create ProtectedRoute.jsx
    Signed-off-by: 2004vivek <119566938+2004vivek@users.noreply.github.com>

---
+++ client/src/component/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+
+import { Navigate } from 'react-router-dom'
+export default function ProtectedRoute({loggedin,children}) {
+   console.log(loggedin);
+  if(loggedin){
+    return children
+  }
+  else{
+    return <Navigate to="/login"/>
+  }
+}
