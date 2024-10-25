8a69afb91ce0d037026453653c436bf3601a0b8c
Vinay Anand Lodhi <vinayanandlodhi12@gmail.com>
2024-10-26 00:53:22 +0530

    Subscribe to newsletter frontend backend done

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -4,7 +4,7 @@ const Profile = require("../Models/Profile");
 const fetchuser = require("../middleware/fetchuser");
 const { body, validationResult } = require("express-validator");
 const { createProfile, fetchProfile, updateprofile } = require("../Controllers/profiles");
-
+const {subscribeNewsletter} = require("../Controllers/OtherController");
 // Route to create a new profile or update an existing one
 router.post("/createprofile", fetchuser, createProfile);
 
@@ -13,5 +13,9 @@ router.get("/fetchprofile", fetchuser,fetchProfile);
 
 // Route to update user's profile
 router.put("/updateprofile", fetchuser,updateprofile);
+
+
+
+router.post('/subscribe',subscribeNewsletter)
 
 module.exports = router;
