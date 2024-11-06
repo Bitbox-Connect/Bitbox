3bbb34f7769e7fe44a89edb5b5a092ea5639cf7a
Anuj3553 <anujverma3553@gmail.com>
2024-11-06 23:55:31 +0530

    Refactor FAQ component for improved readability; update import paths and enhance ScrollTop styling

--- client/src/component/ScrollTop.jsx
+++ client/src/component/ScrollTop.jsx
@@ -34,7 +34,7 @@ const ScrollTop = () => {
                         d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
                     ></path>
                 </svg>
-                <div className="progress-circle"></div> {/* Added progress circle */}
+                <div className="progress-circle"></div>
             </button>
         </div>
     );
