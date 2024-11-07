b51921eeeface77cb1ec7bb78868fb72d58547fa
Vinay Anand Lodhi <vinayanandlodhi12@gmail.com>
2024-11-07 22:53:26 +0530

    upload

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -242,7 +242,7 @@ function Navbar(props) {
                 <ul className="navbar-nav">
                   <div className="Navbar-Btn-Group">
                     {/* Add Project */}
-                    <li>{renderUploadButton()}</li>
+               
 
                     {/* User Profile */}
                     <li
