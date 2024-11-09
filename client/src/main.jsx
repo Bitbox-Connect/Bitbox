8ffa3ee2f2bd8ea4fcdc58d695c4e44e21bb2821
tarunkumar2005 <tarunkumar6258278@gmail.com>
2024-11-09 15:22:55 +0530

    Added Offline Status Indicator

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,18 @@ import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./contexts/authContext/index.jsx";
 // import Alert from "./component/Alert";
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+          .then(registration => {
+              console.log('ServiceWorker registration successful');
+          })
+          .catch(err => {
+              console.log('ServiceWorker registration failed: ', err);
+          });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
